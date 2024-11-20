0e9b73d6235995395a3bba359e8cef85be3e4784
Ihor Belodedov <i.belodedov@rqc.ru>
2024-11-20 23:15:22 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-bg-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-bg-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-text-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-hint-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-hint-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-link-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-link-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-button-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-button-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-button-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-button-text-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-secondary-bg-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-secondary-bg-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-header-bg-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-header-bg-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-bottom-bar-bg-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-bottom-bar-bg-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-accent-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-accent-text-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-section-bg-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-section-bg-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-section-header-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-section-header-text-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-section-separator-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-section-separator-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-subtitle-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-subtitle-text-color</p>
+        </div>
+      </div>
+      <div
+        style={{
+          height: "10px",
+          width: "100%",
+          background: "var(--tg-theme-destructive-text-color)",
+        }}
+      >
+        <div className="textBlock">
+          <p>--tg-theme-destructive-text-color</p>
+        </div>
+      </div>
     </div>
   );
 }
